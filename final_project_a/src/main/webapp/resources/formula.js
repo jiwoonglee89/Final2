0df8c1b0363042983c5d037cf1020f850f6ff562
leewo <leewo@Desktop-lee>
2016-06-29 16:49:57 +0900

    커밋

--- final_project_a/src/main/webapp/resources/formula.js
+++ final_project_a/src/main/webapp/resources/formula.js
@@ -1,6 +1,6 @@
 var th = [ "", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K","L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-function average(){
+function txt_average(){
 	$('.tdselect .textselect').focus().text('=average(');
 }
 function txt_sum(){
@@ -26,4 +26,430 @@ function txt_address(){
 }
 function txt_and(){
 	$('.tdselect .textselect').focus().text('=and(');
+}
+function txt_asinh(){
+	$('.tdselect .textselect').focus().text('=asinh(');
+}
+function txt_atan(){
+	$('.tdselect .textselect').focus().text('=atan(');
+}
+function txt_atan2(){
+	$('.tdselect .textselect').focus().text('=atan2(');
+}
+function txt_atanh(){
+	$('.tdselect .textselect').focus().text('=atanh(');
+}
+function txt_bin2dec(){
+	$('.tdselect .textselect').focus().text('=bin2dec(');
+}
+function txt_bin2hex(){
+	$('.tdselect .textselect').focus().text('=bin2hex(');
+}
+function txt_bin2oct(){
+	$('.tdselect .textselect').focus().text('=bin2oct(');
+}
+function txt_binomdist(){
+	$('.tdselect .textselect').focus().text('=bin2oct(');
+}
+function txt_ceiling(){
+	$('.tdselect .textselect').focus().text('=ceiling(');
+}
+function txt_char(){
+	$('.tdselect .textselect').focus().text('=char(');
+}
+function txt_chidist(){
+	$('.tdselect .textselect').focus().text('=chidist(');
+}
+function txt_code(){
+	$('.tdselect .textselect').focus().text('=code(');
+}
+function txt_column(){
+	$('.tdselect .textselect').focus().text('=column(');
+}
+function txt_columns(){
+	$('.tdselect .textselect').focus().text('=columns(');
+}
+function txt_combin(){
+	$('.tdselect .textselect').focus().text('=combin(');
+}
+function txt_concatenate(){
+	$('.tdselect .textselect').focus().text('=concatenate(');
+}
+function txt_confidence(){
+	$('.tdselect .textselect').focus().text('=confidence(');
+}
+function txt_correl(){
+	$('.tdselect .textselect').focus().text('=correl(');
+}
+function txt_cos(){
+	$('.tdselect .textselect').focus().text('=cos(');
+}
+function txt_cosh(){
+	$('.tdselect .textselect').focus().text('=cosh(');
+}
+function txt_count(){
+	$('.tdselect .textselect').focus().text('=count(');
+}
+function txt_counta(){
+	$('.tdselect .textselect').focus().text('=counta(');
+}
+function txt_countblank(){
+	$('.tdselect .textselect').focus().text('=countblank(');
+}
+function txt_countif(){
+	$('.tdselect .textselect').focus().text('=countif(');
+}
+function txt_covar(){
+	$('.tdselect .textselect').focus().text('=covar(');
+}
+function txt_date(){
+	$('.tdselect .textselect').focus().text('=date(');
+}
+function txt_day(){
+	$('.tdselect .textselect').focus().text('=day(');
+}
+function txt_dec2bin(){
+	$('.tdselect .textselect').focus().text('=dec2bin(');
+}
+function txt_dec2hex(){
+	$('.tdselect .textselect').focus().text('=dec2hex(');
+}
+function txt_dec2oct(){
+	$('.tdselect .textselect').focus().text('=dec2oct(');
+}
+function txt_degrees(){
+	$('.tdselect .textselect').focus().text('=degrees(');
+}
+function txt_even(){
+	$('.tdselect .textselect').focus().text('=even(');
+}
+function txt_exp(){
+	$('.tdselect .textselect').focus().text('=exp(');
+}
+function txt_expondist(){
+	$('.tdselect .textselect').focus().text('=expondist(');
+}
+function txt_fact(){
+	$('.tdselect .textselect').focus().text('=fact(');
+}
+function txt_false(){
+	$('.tdselect .textselect').focus().text('=false(');
+}
+function txt_fdist(){
+	$('.tdselect .textselect').focus().text('=fdist(');
+}
+function txt_find(){
+	$('.tdselect .textselect').focus().text('=find(');
+}
+function txt_fixed(){
+	$('.tdselect .textselect').focus().text('=fixed(');
+}
+function txt_floor(){
+	$('.tdselect .textselect').focus().text('=floor(');
+}
+function txt_ftest(){
+	$('.tdselect .textselect').focus().text('=ftest(');
+}
+function txt_gammadist(){
+	$('.tdselect .textselect').focus().text('=gammadist(');
+}
+function txt_gammaln(){
+	$('.tdselect .textselect').focus().text('=gammadist(');
+}
+function txt_gcd(){
+	$('.tdselect .textselect').focus().text('=gcd(');
+}
+function txt_geomean(){
+	$('.tdselect .textselect').focus().text('=geomean(');
+}
+function txt_harmean(){
+	$('.tdselect .textselect').focus().text('=harmean(');
+}
+function txt_hex2bin(){
+	$('.tdselect .textselect').focus().text('=hex2bin(');
+}
+function txt_hex2dec(){
+	$('.tdselect .textselect').focus().text('=hex2dec(');
+}
+function txt_hex2oct(){
+	$('.tdselect .textselect').focus().text('=hex2oct(');
+}
+function txt_hlookup(){
+	$('.tdselect .textselect').focus().text('=hlookup(');
+}
+function txt_hour(){
+	$('.tdselect .textselect').focus().text('=hour(');
+}
+function txt_hypgeomdist(){
+	$('.tdselect .textselect').focus().text('=hypgeomdist(');
+}
+function txt_index(){
+	$('.tdselect .textselect').focus().text('=index(');
+}
+function txt_if(){
+	$('.tdselect .textselect').focus().text('=if(');
+}
+function txt_iferror(){
+	$('.tdselect .textselect').focus().text('=iferror(');
+}
+function txt_int(){
+	$('.tdselect .textselect').focus().text('=int(');
+}
+function txt_intercept(){
+	$('.tdselect .textselect').focus().text('=intercept(');
+}
+function txt_isblank(){
+	$('.tdselect .textselect').focus().text('=isblank(');
+}
+function txt_iserr(){
+	$('.tdselect .textselect').focus().text('=iserr(');
+}
+function txt_iserror(){
+	$('.tdselect .textselect').focus().text('=iserror(');
+}
+function txt_isna(){
+	$('.tdselect .textselect').focus().text('=isna(');
+}
+function txt_isnumber(){
+	$('.tdselect .textselect').focus().text('=isnumber(');
+}
+function txt_istext(){
+	$('.tdselect .textselect').focus().text('=istext(');
+}
+function txt_kurt(){
+	$('.tdselect .textselect').focus().text('=kurt(');
+}
+function txt_large(){
+	$('.tdselect .textselect').focus().text('=large(');
+}
+function txt_lcm(){
+	$('.tdselect .textselect').focus().text('=lcm(');
+}
+function txt_left(){
+	$('.tdselect .textselect').focus().text('=left(');
+}
+function txt_len(){
+	$('.tdselect .textselect').focus().text('=len(');
+}
+function txt_ln(){
+	$('.tdselect .textselect').focus().text('=ln(');
+}
+function txt_log(){
+	$('.tdselect .textselect').focus().text('=log(');
+}
+function txt_log10(){
+	$('.tdselect .textselect').focus().text('=log10(');
+}
+function txt_lookup(){
+	$('.tdselect .textselect').focus().text('=lookup(');
+}
+function txt_lower(){
+	$('.tdselect .textselect').focus().text('=lower(');
+}
+function txt_match(){
+	$('.tdselect .textselect').focus().text('=match(');
+}
+function txt_median(){
+	$('.tdselect .textselect').focus().text('=median(');
+}
+function txt_mid(){
+	$('.tdselect .textselect').focus().text('=mid(');
+}
+function txt_minute(){
+	$('.tdselect .textselect').focus().text('=minute(');
+}
+function txt_mod(){
+	$('.tdselect .textselect').focus().text('=mod(');
+}
+function txt_mode(){
+	$('.tdselect .textselect').focus().text('=mode(');
+}
+function txt_month(){
+	$('.tdselect .textselect').focus().text('=month(');
+}
+function txt_na(){
+	$('.tdselect .textselect').focus().text('=na(');
+}
+function txt_normdist(){
+	$('.tdselect .textselect').focus().text('=normdist(');
+}
+function txt_normsdist(){
+	$('.tdselect .textselect').focus().text('=normsdist(');
+}
+function txt_not(){
+	$('.tdselect .textselect').focus().text('=not(');
+}
+function txt_now(){
+	$('.tdselect .textselect').focus().text('=now()');
+}
+function txt_oct2bin(){
+	$('.tdselect .textselect').focus().text('=oct2bin(');
+}
+function txt_oct2dec(){
+	$('.tdselect .textselect').focus().text('=oct2dec(');
+}
+function txt_oct2hex(){
+	$('.tdselect .textselect').focus().text('=oct2hex(');
+}
+function txt_odd(){
+	$('.tdselect .textselect').focus().text('=odd(');
+}
+function txt_or(){
+	$('.tdselect .textselect').focus().text('=or(');
+}
+function txt_pearson(){
+	$('.tdselect .textselect').focus().text('=pearson(');
+}
+function txt_percentile(){
+	$('.tdselect .textselect').focus().text('=percentile(');
+}
+function txt_percentrank(){
+	$('.tdselect .textselect').focus().text('=percentrank(');
+}
+function txt_permut(){
+	$('.tdselect .textselect').focus().text('=permut(');
+}
+function txt_pi(){
+	$('.tdselect .textselect').focus().text('=pi()');
+}
+function txt_poisson(){
+	$('.tdselect .textselect').focus().text('=poisson(');
+}
+function txt_power(){
+	$('.tdselect .textselect').focus().text('=power(');
+}
+function txt_product(){
+	$('.tdselect .textselect').focus().text('=product(');
+}
+function txt_radians(){
+	$('.tdselect .textselect').focus().text('=radians(');
+}
+function txt_rand(){
+	$('.tdselect .textselect').focus().text('=rand()');
+}
+function txt_rank(){
+	$('.tdselect .textselect').focus().text('=rank(');
+}
+function txt_replace(){
+	$('.tdselect .textselect').focus().text('=replace');
+}
+function txt_rept(){
+	$('.tdselect .textselect').focus().text('=rept(');
+}
+function txt_right(){
+	$('.tdselect .textselect').focus().text('=right(');
+}
+function txt_round(){
+	$('.tdselect .textselect').focus().text('=round(');
+}
+function txt_rounddown(){
+	$('.tdselect .textselect').focus().text('=rounddown(');
+}
+function txt_roundup(){
+	$('.tdselect .textselect').focus().text('=roundup(');
+}
+function txt_row(){
+	$('.tdselect .textselect').focus().text('=row(');
+}
+function txt_rows(){
+	$('.tdselect .textselect').focus().text('=rows(');
+}
+function txt_search(){
+	$('.tdselect .textselect').focus().text('=search(');
+}
+function txt_second(){
+	$('.tdselect .textselect').focus().text('=second(');
+}
+function txt_sign(){
+	$('.tdselect .textselect').focus().text('=sign(');
+}
+function txt_sin(){
+	$('.tdselect .textselect').focus().text('=sin(');
+}
+function txt_sinh(){
+	$('.tdselect .textselect').focus().text('=sinh(');
+}
+function txt_skew(){
+	$('.tdselect .textselect').focus().text('=skew(');
+}
+function txt_slope(){
+	$('.tdselect .textselect').focus().text('=slope(');
+}
+function txt_small(){
+	$('.tdselect .textselect').focus().text('=small(');
+}
+function txt_sqrt(){
+	$('.tdselect .textselect').focus().text('=sqrt(');
+}
+function txt_sqrtpi(){
+	$('.tdselect .textselect').focus().text('=sqrtpi(');
+}
+function txt_standardize(){
+	$('.tdselect .textselect').focus().text('=standardize(');
+}
+function txt_stdev(){
+	$('.tdselect .textselect').focus().text('=stdev(');
+}
+function txt_substitute(){
+	$('.tdselect .textselect').focus().text('=substitute(');
+}
+function txt_sumif(){
+	$('.tdselect .textselect').focus().text('=sumif(');
+}
+function txt_sumproduct(){
+	$('.tdselect .textselect').focus().text('=sumproduct(');
+}
+function txt_sumsq(){
+	$('.tdselect .textselect').focus().text('=sumsq(');
+}
+function txt_tan(){
+	$('.tdselect .textselect').focus().text('=tan(');
+}
+function txt_tanh(){
+	$('.tdselect .textselect').focus().text('=tanh(');
+}
+function txt_tdist(){
+	$('.tdselect .textselect').focus().text('=tdist(');
+}
+function txt_text(){
+	$('.tdselect .textselect').focus().text('=text(');
+}
+function txt_time(){
+	$('.tdselect .textselect').focus().text('=time(');
+}
+function txt_today(){
+	$('.tdselect .textselect').focus().text('=today()');
+}
+function txt_trim(){
+	$('.tdselect .textselect').focus().text('=trim(');
+}
+function txt_true(){
+	$('.tdselect .textselect').focus().text('=true()');
+}
+function txt_trunc(){
+	$('.tdselect .textselect').focus().text('=trunc(');
+}
+function txt_ttest(){
+	$('.tdselect .textselect').focus().text('=ttest(');
+}
+function txt_upper(){
+	$('.tdselect .textselect').focus().text('=upper(');
+}
+function txt_value(){
+	$('.tdselect .textselect').focus().text('=value(');
+}
+function txt_var(){
+	$('.tdselect .textselect').focus().text('=var(');
+}
+function txt_vlookup(){
+	$('.tdselect .textselect').focus().text('=vlookup(');
+}
+function txt_weekday(){
+	$('.tdselect .textselect').focus().text('=weekday(');
+}
+function txt_weibull(){
+	$('.tdselect .textselect').focus().text('=weibull(');
+}
+function txt_year(){
+	$('.tdselect .textselect').focus().text('=year(');
 }
