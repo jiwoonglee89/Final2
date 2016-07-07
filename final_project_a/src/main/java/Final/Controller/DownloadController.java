fc420f34a50fa97a78ac5376be4e14e76d277d8b
leewo <leewo@Desktop-lee>
2016-07-07 20:02:33 +0900

    다운로드 수정

--- final_project_a/src/main/java/Final/Controller/DownloadController.java
+++ final_project_a/src/main/java/Final/Controller/DownloadController.java
@@ -9,26 +9,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DownloadController {
+	private WebApplicationContext  context = null;
+	
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.context=(WebApplicationContext)applicationContext;
+	}
+	@RequestMapping(value="/download.do")
+	public ModelAndView download(HttpServletRequest request) throws Exception{
+		File downloadFile = getFile(request);
+		return new ModelAndView("downView", "downloadFile", downloadFile);
+		
+	}
+	private File getFile(HttpServletRequest request) {
+		//String path = context.getServletContext().getRealPath("/WEB-INF/Ό³Έν.txt");
+		String path="C:\\final_test\\"+session_Title(request)+".xlsx";
+		return new File(path);
+	}
 	public String session_Title(HttpServletRequest request){
 		HttpSession session = request.getSession();
 		String title = (String) session.getAttribute("title");
 		return title;
-	}
-	@RequestMapping(value="/download.do")
-	public ModelAndView save(HttpServletRequest request) throws IOException {
-		File file = new File("C:\\final_test\\"+session_Title(request)+".xlsx");
-		FileInputStream fis = new FileInputStream(file);
-		XSSFWorkbook workbook = new XSSFWorkbook(fis);
-		
-		HashMap download_data=new HashMap();
-		download_data.put("title", session_Title(request));
-		download_data.put("d_workbook", workbook);
-		return new ModelAndView("downView", "download_data", download_data);
 	}
 }
